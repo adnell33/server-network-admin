2eb4e0235058328a25d4027f1184c9936dbfe61f
adnell33 <adrianbongsor@gmail.com>
2024-02-20 00:54:24 -0500

    test pembagian

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ def addition(a, b):
 	return result
 def main():
 	print(addition(20,20))
+	print(divide(4,2))
+def divide (a,b):
+	result = (a/b)
+	return result 
 
 if __name__ == "__main__":
 	main()
+
+#test tambah pembagian 
+
